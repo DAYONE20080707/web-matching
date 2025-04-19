ae5789f33c32360201609c03e2ec49c3912972f5
Tsukimori <tsukimori@day-1.tokyo>
2025-04-19 17:05:44 +0900

    画像削除

--- components/main/top/TopFeatureList.tsx
+++ components/main/top/TopFeatureList.tsx
@@ -25,13 +25,13 @@ const TopFeatureList = () => {
           <TopFeatureItem
             subTitle="Feature 02"
             mainTitle="信頼できる“実績あるプロ”だけを厳選"
-            imageSrc="/top/top-feature-01.jpg"
+            imageSrc="/top/top-feature-02.jpg"
             body="ご紹介するのは、補助金申請の実績や専門分野が明確な登録パートナーのみ。初めてでも安心してご相談いただけます。"
           />
           <TopFeatureItem
             subTitle="Feature 03"
             mainTitle="マッチング後のやりとりもスムーズ"
-            imageSrc="/top/top-feature-01.jpg"
+            imageSrc="/top/top-feature-03.jpg"
             body="紹介された中から気になる専門家とすぐ連絡可能。やりとりはスピーディー＆効率的だから、申請準備もぐっと楽になります。"
           />
         </div>
